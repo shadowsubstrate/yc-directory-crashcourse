1fca120e1b8ff5d1ead8ac04e7a0375a0b6a1dbf
shadowsubstrate <shadowsubstrate@gmail.com>
2024-11-29 00:51:45 +0100

    Added Sanity structures for users/authors and startups

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
+import { author } from "@/sanity/schemaTypes/author";
 import { type SchemaTypeDefinition } from "sanity";
+import { startup } from "./startup";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, startup],
 };
